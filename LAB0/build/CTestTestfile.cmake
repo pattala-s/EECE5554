fd3ab9ada0d0ba70f08a9f16d8e286b0d8aeb1b9
pattala.s <pattala.s@northeastern.edu>
2023-02-23 14:20:50 -0500

    commit2

--- LAB0/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/srinidhi/EECE5554/LAB1/src
-# Build directory: /home/srinidhi/EECE5554/LAB1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("gps_driver")
--- LAB0/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/srinidhi/EECE5554/LAB1/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/srinidhi/EECE5554/LAB1/devel;/opt/ros/noetic'.split(';') if '/home/srinidhi/EECE5554/LAB1/devel;/opt/ros/noetic' != '' else []
